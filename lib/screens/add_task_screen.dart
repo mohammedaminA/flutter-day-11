442ab5f3ffe0f702b2781f50f9411d204efafe70
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-05-17 01:53:13 +0300

    Add Task Screen

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 class AddTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Color(0xff757575),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0))
+        ),
+      ),
+    );
   }
 }
