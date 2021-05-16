72bf306bdde9a9202b6617ac003d9fd095cb1834
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-05-17 02:27:03 +0300

    Add Task Screen

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -6,9 +6,37 @@ class AddTaskScreen extends StatelessWidget {
     return Container(
       color: Color(0xff757575),
       child: Container(
+        padding: EdgeInsets.all(10.0),
         decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0))
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20.0),
+                topRight: Radius.circular(20.0))),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.lightBlueAccent,
+                fontSize: 39.0,
+              ),
+            ),
+            TextField(
+              textAlign: TextAlign.center,
+              autofocus: true,
+            ),
+            FlatButton(
+                child: Text(
+                  'Add Task',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.lightBlueAccent,
+                onPressed: () {
+                  //Add Some Functionality
+                })
+          ],
         ),
       ),
     );
