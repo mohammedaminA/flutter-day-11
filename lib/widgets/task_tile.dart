a772299bc1f42cb6472ed657b98952ae101311ad
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-05-17 12:55:51 +0300

    State Management

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -9,29 +9,33 @@ class TaskTile extends StatefulWidget {
 class _TaskTileState extends State<TaskTile> {
   bool isChecked = false;
 
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('This is a task.', style: TextStyle(decoration: isChecked ? TextDecoration.lineThrough : null),),
-      trailing: TaskCheckBox(isChecked),
+      trailing: TaskCheckBox(checkBoxState: isChecked, toggleCheckBoxState:(bool checkBoxState) {
+        setState(() {
+          isChecked = checkBoxState;
+        });
+      }),
     );
   }
 }
 
 class TaskCheckBox extends StatelessWidget {
 
-  TaskCheckBox(this.checkBoxState);
+  TaskCheckBox({this.checkBoxState, this.toggleCheckBoxState});
 
  final bool checkBoxState;
+ final Function toggleCheckBoxState;
 
   @override
   Widget build(BuildContext context) {
     return Checkbox(
       activeColor: Colors.lightBlueAccent,
       value: checkBoxState,
-      onChanged: (newValue) {
-
-      }
+      onChanged: toggleCheckBoxState,
     );
   }
 }
