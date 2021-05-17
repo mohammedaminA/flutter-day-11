2c8c4892f4c057dc1579fb165382af0a0b035020
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-05-17 13:47:25 +0300

    Refactor

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:todoey/widgets/task_tile.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+
+  List<Task> tasks = [];
   @override
   Widget build(BuildContext context) {
     return ListView(
