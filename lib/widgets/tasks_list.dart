d6aa7ad767b251fd608a89809d5809d798c329f8
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-05-17 14:24:49 +0300

    Refactor

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -9,14 +9,20 @@ class TasksList extends StatefulWidget {
 
 class _TasksListState extends State<TasksList> {
 
-  List<Task> tasks = [];
+  List<Task> tasks = [
+    Task(name:  'Buy Milk'),
+    Task(name: 'Do Chores'),
+    Task(name: 'Buy Eggs'),
+    Task(name: 'Code the app')
+  ];
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget> [
-        TaskTile(),
-        TaskTile(),
-        TaskTile()
+        TaskTile(taskText: tasks[0].name, isChecked: tasks[0].isDone,),
+        TaskTile(taskText: tasks[1].name, isChecked: tasks[1].isDone,),
+        TaskTile(taskText: tasks[2].name, isChecked: tasks[2].isDone,),
+        TaskTile(taskText: tasks[3].name, isChecked: tasks[3].isDone,)
       ],
     );
   }
