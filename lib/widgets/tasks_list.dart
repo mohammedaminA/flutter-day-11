44f86044568df82107d0ca99261714e016621903
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-05-17 21:58:39 +0300

    State Management

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -2,13 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:todoey/widgets/task_tile.dart';
 import 'package:provider/provider.dart';
 import 'package:todoey/models/task_data.dart';
-class TasksList extends StatefulWidget {
-
-  @override
-  _TasksListState createState() => _TasksListState();
-}
-
-class _TasksListState extends State<TasksList> {
+class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
